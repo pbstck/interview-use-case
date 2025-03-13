0731d72b715a056508ba223d0047790e56d1d15a
Thomas Muzy <tmuzy@pubstack.io>
2025-03-13 17:57:02 +0100

    fix: cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import express, {Express} from 'express';
 import { ContextResource } from './resource/context';
 import { ConfigResolverService } from './service/configResolver';
 import { HealthCheckResource } from './resource/health';
+import cors from 'cors';
 
 const app: Express = express();
 const port = 3000;
@@ -13,8 +14,8 @@ const servicePack = {
 }
 
 router.use('/context', ContextResource(servicePack)).use('/status', HealthCheckResource);
+app.use(cors())
 app.use('/api', router);
-
 app.listen(port, () => {
   console.log(`Api server is running on port ${port}`);
 });
